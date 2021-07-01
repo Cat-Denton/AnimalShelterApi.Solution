8e8d6465d62d4c52d8bcf2fad0b05bfce1adebd6
Cat-Denton <willwdenton@gmail.com>
2021-07-01 14:43:58 -0700

    Create Animal model and initial migration

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AnimalShelterApi.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public DateTime Birthdate { get; set; }
+  }
+}
