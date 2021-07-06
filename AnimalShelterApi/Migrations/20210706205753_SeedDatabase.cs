5017a73ed517d315a5872d6fd45c7f6eb29f23a9
Cat-Denton <willwdenton@gmail.com>
2021-07-06 14:00:02 -0700

    add database seed for animal data

---
+++ AnimalShelterApi/Migrations/20210706205753_SeedDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Birthdate", "Gender", "ImageUrl", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Male", "imgur.com/hiccupcat", "Hiccup", "Cat" },
+                    { 2, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Female", "imgur.com/zipdog", "Zipper", "Dog" },
+                    { 3, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Female", "imgur.com/coralcat", "Coraline", "Cat" },
+                    { 4, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Male", "imgur.com/windoge", "Winchester", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -1,14 +1,24 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelterApi.Models
 {
   public class AnimalShelterApiContext : DbContext
   {
+    public DbSet<Animal> Animals { get; set; }
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options)
       : base(options)
     {
     }
-
-    public DbSet<Animal> Animals { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal{AnimalId = 1, Name = "Hiccup", Species = "Cat", Gender = "Male", Birthdate = Convert.ToDateTime("2015-01-01T00:00:00"), ImageUrl = "imgur.com/hiccupcat" },
+          new Animal{AnimalId = 2, Name = "Zipper", Species = "Dog", Gender = "Female", Birthdate = Convert.ToDateTime("2015-01-01T00:00:00"), ImageUrl = "imgur.com/zipdog" },
+          new Animal{AnimalId = 3, Name = "Coraline", Species = "Cat", Gender = "Female", Birthdate = Convert.ToDateTime("2015-01-01T00:00:00"), ImageUrl = "imgur.com/coralcat" },
+          new Animal{AnimalId = 4, Name = "Winchester", Species = "Dog", Gender = "Male", Birthdate = Convert.ToDateTime("2015-01-01T00:00:00"), ImageUrl = "imgur.com/windoge" }
+        );
+    }
   }
 }
